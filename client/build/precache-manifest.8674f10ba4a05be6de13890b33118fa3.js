d799bd2ccf8747d753e7d27f1352f23934193e6b
Tolupunoori Rajshekar <69853022+hacktronicsraj@users.noreply.github.com>
2023-07-19 22:15:12 +0530

    Add files via upload

---
+++ client/build/precache-manifest.8674f10ba4a05be6de13890b33118fa3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ffb3c93534a137ea25e63caa33ddc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93bd5e7feb5b98706e41",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "ea51b08ea49af300ce10",
+    "url": "/static/css/main.242a1f8f.chunk.css"
+  },
+  {
+    "revision": "93bd5e7feb5b98706e41",
+    "url": "/static/js/2.3df430fd.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.3df430fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea51b08ea49af300ce10",
+    "url": "/static/js/main.e09500e5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
